4727bf7ed1dbe18a0c24830698764475ad79b503
JP Kohrs <jpkohrs@gmail.com>
2023-07-05 12:16:01 +0200

    refined LanguageSelect to be a purer component

--- theme/tailwind.config.js
+++ theme/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-    theme: {
-        extend: {},
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      papaya: '#ff710b',
     },
-    plugins: [],
+  },
+  plugins: [],
 }
