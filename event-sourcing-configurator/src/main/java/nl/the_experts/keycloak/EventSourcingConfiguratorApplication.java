d80df8a8ee2720e9225de611fdd076561a98e94f
Maik Kingma <maikkingma@kingmas.me>
2024-05-15 08:23:41 +0200

    working event sourcing implemenation

---
+++ event-sourcing-configurator/src/main/java/nl/the_experts/keycloak/EventSourcingConfiguratorApplication.java
@@ -0,0 +1,15 @@
+package nl.the_experts.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan(basePackages = {"org.axonframework.eventsourcing.eventstore.jpa", "nl.the_experts.keycloak.data"})
+public class EventSourcingConfiguratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSourcingConfiguratorApplication.class, args);
+	}
+
+}
---
+++ event-sourcing-configurator/src/test/java/nl/the_experts/keycloak/EventSourcingConfiguratorApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.the_experts.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSourcingConfiguratorApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
